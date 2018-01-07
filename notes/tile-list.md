9c26ad9528164b2eb55f89339a8b732c23f30763
kmpm <peter@birchroad.net>
2018-01-07 20:05:25 +0100

    More exploration of TileList

--- notes/tile-list.md
+++ notes/tile-list.md
@@ -32,15 +32,36 @@ Official documentation can be found at https://dev.fitbit.com/build/guides/desig
 </svg>
 ```
 ## Configuration
-__Variables__
+### Variables
 * `align-mode`: X='0' selects no alignment, i.e. the default behavior. 
 X='1' selects aligning the nearest item to the top of the view after the scroll ends.
 * `peek-enabled`: X='0' disables peek.
 * `reorder-enabled`: 1= Allow reordering of items (default). 0= Disable reordering.
 * `separator-height-bottom`: 2= Seen used in BART example
 
-__Sub Elements__
+### Sub Elements
+The component is composed of the following SVG sub elements:
 * [#title-list-item](#title-list-item): A repeating container element.
+
+## Javascript
+The tile-list element implements a 
+[TileList Interface](https://dev.fitbit.com/build/reference/device-api/document/#interface-tilelist-)
+
+### Attributes
+* length: Returns number of tiles in list
+* text:
+* image:
+* value: 
+* mode:
+* state:
+
+### Methods
+* `getTile(position: number)`: Returns TileItem
+* `insertTile(item: TitleListItem, position: number, options?: TitleListItemOperationOptions | undefined)`
+* `obtainTile()`: TileListItem factory that returns an item suitable for using with `insertTile()`
+* addEventListener
+* removeEventListener
+* dispatchEvent
 
 
 # title-list-item
@@ -55,9 +76,27 @@ system-events="all">
 ```
 
 ## Configuration
-__Attributes__
+### Attributes
 
 * `focusable="<bool>"`: Seen in example
 * `pointer-events="visible"`: Seen in example
 * `system-events="all"`: Seen in example 
 
+### Sub Elements
+
+The component is composed of the following SVG sub elements:
+
+## Javascript
+Implements [TileListItem Interface](https://dev.fitbit.com/build/reference/device-api/document/#interface-tilelistitem-)
+### Attributes
+* text
+* image
+* value
+* mode 
+* state
+
+### Methods
+* `align()`:
+* `hide()`:
+* `remove()`: removes the item from list
+* `show()`: 
--- samples/TileTest/app/index.js
+++ samples/TileTest/app/index.js
@@ -3,23 +3,70 @@
  */
 let document = require('document');
 
+import { dumpObject } from './util';
+
 console.log('App Started');
 
 
-function renumber() {
-  console.log('renumber');
-  let list = document.getElementById('mylist');
-  
-  if (!list) {
-    console.log('no list found');
-    return;
-  }
-  
-  let tiles = list.getElementsByClassName('item');
+var tiles;
 
-  tiles.forEach((tile, i) => {
-    tile.getElementById('sampleText').text = tile.id + '-jox-' + i.toString();
+function updateList() {
+  console.log('updateList()')
+  //update the text element on each item to show it's id and index in the array.
+  tiles.forEach((tile, index) => {
+    setText(tile, tile.id + ', index:' + index.toString());
+    //console.log('tile:', JSON.stringify(tile));
+    //tile.onactivate(handlerFactory(index));
   });
 }
 
-renumber();
+function addItem(tileType) {
+  console.log('addItem()');
+  let tile = list.obtainTile(tileType);
+  console.log('expected:' + tileType + ', actual:' + typeof tile + ':' + tile);
+  if (tile) {
+    dumpObject(tile);
+    list.insertTile(tile, 0);
+    updateList();
+  }
+}
+
+
+function setText(tileElement, text) {
+  tileElement.getElementById('sampleText').text = text;
+}
+
+function handlerFactory(index) {
+  var tile = tiles[index];
+  return function onActivate(evt) {
+    setText(tile, 'Clicked ' + index.toString());
+  }
+}
+
+function dumpTile(position) {
+  console.log(`dumpTile(${position})`);
+  if (typeof list.getTile === 'function') {
+    var t = list.getTile(position);
+    dumpObject(t);
+  }
+  else {
+    console.log('dump list');
+    dumpObject(list);
+  }
+}
+
+let list = document.getElementById('mylist');   
+tiles = list.getElementsByClassName('item');
+
+
+
+//update the list after 2 seconds
+setTimeout(function() {
+  updateList();
+  dumpTile(0);
+  addItem('tile-list-item');
+  addItem('#tile-list-item');
+  addItem('TileListItem');
+  
+  
+}, 2000);
